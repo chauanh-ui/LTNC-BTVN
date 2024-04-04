96fb79862ecfe30a801556452af5a806aeecf6ce
chauanh-ui <143055160+chauanh-ui@users.noreply.github.com>
2024-04-04 10:34:12 +0700

    Update DrawingBook.cpp

---
+++ LTNC-04/DrawingBook.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+int pageCount(int n, int p) {
+
+}
