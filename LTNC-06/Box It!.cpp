c90b4116a589dce7f11a599b996c3ca12b29f1fa
chauanh-ui <143055160+chauanh-ui@users.noreply.github.com>
2024-05-23 15:08:29 +0700

    Update Box It!.cpp

---
+++ LTNC-06/Box It!.cpp
@@ -0,0 +1,55 @@
+class Box 
+{
+private:
+    int l, b, h;
+public:
+    Box()
+    {
+        l = 0;
+        b = 0;
+        h = 0;
+    }
+    
+    Box(int length, int breadth, int height)
+    {
+        l = length;
+        b = breadth;
+        h = height;
+    }  
+    
+    Box(const Box& B)
+    {
+        l = B.l;
+        b = B.b;
+        h = B.h;
+    }
+    
+    int getLength()
+    {
+        return l;
+    }
+    int getBreadth()
+    {
+        return b;
+    }
+    int getHeight()
+    {
+        return h;
+    }
+    long long CalculateVolume()
+    {
+        return (long long) l * b * h;
+    }
+    
+    friend bool operator <(const Box& A, const Box& B)
+    {
+        if(A.l < B.l || (A.b < B.b && A.l == B.l) || (A.h < B.h && A.b == B.b && A.l == B.l)) return true;
+        else return false;
+    }
+    
+    friend ostream& operator << (ostream& os, const Box& B)
+    {
+        os << B.l << " " << B.b << " " << B.h;
+        return os;
+    }
+};
